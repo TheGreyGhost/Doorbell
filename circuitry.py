34c263d9e9505a15f6832896a452c9a5cd50a3a7
TheGreyGhost <5291905+TheGreyGhost@users.noreply.github.com>
2019-01-06 20:45:00 +1030

    added status pin

--- circuitry.py
+++ circuitry.py
@@ -1,16 +1,21 @@
 from gpiozero import LED, Button
 import time
 
-doorbell_button = Button(pin=17, hold_time=0.1)  #Connect one side of the button to a ground pin and the other to the pin
+LED_BLINK_DELAY = 1.0   # blinks on and off at this rate; eg DELAY = 1.0 -> on for 1 s, off for 1 s, etc.
+BUTTON_HOLD_TIME = 0.1  # debounce for this length of time (s)
+doorbell_button = Button(pin=17, hold_time=BUTTON_HOLD_TIME)  #Connect one side of the button to a ground pin and the other to the pin
 speakers_relay = LED(pin=18)  # output is 0V or 3.3 V
+status_led = LED(pin=27)
 
 def waitForButtonPress():
     """
-    Wait until the doorbell button has been held for at least
+    Wait until the doorbell button has been held for at least BUTTON_HOLD_TIME.
+    Blinks the status led while waiting.
     :return:
     """
     while (True):
-        doorbell_button.wait_for_press()
+        status_led.toggle()
+        doorbell_button.wait_for_press(timeout=LED_BLINK_DELAY)
         while (doorbell_button.is_pressed):
             if doorbell_button.is_held:
                 return
