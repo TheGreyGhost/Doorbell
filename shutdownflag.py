3ad5d523e35d651cd1e4ee328fd3e407f3251429
TheGreyGhost <5291905+TheGreyGhost@users.noreply.github.com>
2024-10-22 15:59:03 +1030

    Create shutdownflag.py

---
+++ shutdownflag.py
@@ -0,0 +1,10 @@
+import signal
+
+class ShutdownFlag:
+  shutdown_triggered = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self, signum, frame):
+    self.shutdown_triggered = True
